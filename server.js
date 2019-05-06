bf5dcf918bd2b3cd50ad533b628db3bb6d3ea002
Jacob Sipple <sipple3@gmail.com>
2019-05-05 21:23:59 -0400

    changed server package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080 ;
 const app = express();
 const mongoose = require('mongoose')
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/books";
